03b4e631d0340e57384baf01d0d4d74f88aa5206
Zack Young <zack.young@spiff.com>
2021-12-03 23:46:49 -0700

    Add Day 04 solutions

---
+++ day04/pt2.rb
@@ -0,0 +1,75 @@
+require "set"
+
+class Board
+  def initialize
+    @coords = {}
+    @numbers = Array.new(5) { Array.new(5, 0) }
+    @marked = Array.new(5) { Array.new(5, false) }
+    @number_count = 0
+  end
+
+  def add(number)
+    row = @number_count / 5
+    column = @number_count % 5
+    @numbers[row][column] = number
+    @coords[number] = [row, column]
+    @number_count += 1
+  end
+
+  def full?
+    @number_count == 25
+  end
+
+  def mark(number)
+    (row, column) = @coords[number]
+    @marked[row][column] = true
+    row_finished = (0..4).all? { |i_col| @marked[row][i_col] }
+    return true if row_finished
+    col_finished = (0..4).all? { |i_row| @marked[i_row][column] }
+  end
+
+  def score
+    score = 0
+    (0..4).each do |row|
+      (0..4).each do |column|
+        number = @numbers[row][column]
+        score += number unless @marked[row][column]
+      end
+    end
+    score
+  end
+end
+
+boards = []
+board_indexes = Hash.new { |h, k| h[k] = [] }
+called_numbers = []
+File.readlines('input')
+    .map do |line|
+      called_numbers = line.strip.split(',').map(&:to_i)
+    end
+
+board = Board.new
+File.readlines('input_boards')
+.map do |row|
+  row.strip.split(" ").map(&:to_i).each { |number|
+    board.add number
+    board_indexes[number] << board
+  }
+  if board.full?
+    boards << board
+    board = Board.new
+  end
+end
+
+finished_boards = Set.new
+called_numbers.each { |called_number|
+  board_indexes[called_number].each { |board|
+    next if finished_boards.include? board
+    board_won = board.mark(called_number)
+    if board_won
+      score = board.score
+      finished_boards << board
+      pp "Board won! #{called_number} * #{score} = #{called_number * score}"
+    end
+  }
+}
