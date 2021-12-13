45b44b81d98b451716987195feb3176bebfb57b5
Zack Young <zack.young@spiff.com>
2021-12-12 23:13:16 -0700

    Add Day 13 solutions

---
+++ day13/pt2.rb
@@ -0,0 +1,34 @@
+require 'set'
+
+graph = Array.new(895) { Array.new(1309) }
+
+File.readlines('input')
+    .each do |line|
+      col, row = line.strip.split(',').map(&:to_i)
+      graph[row][col] = 1
+    end
+
+instructions = File.readlines('input_instr')
+    .map do |line|
+      line.strip.split(' ')[2].split("=")
+    end
+
+instructions.each { |axis, index|
+  index = index.to_i
+  if axis == "x"
+    graph.each { |row|
+      right_side = row.slice!(index..)
+      right_side.drop(1).each_with_index { |value, i|
+        row[-(i + 1)] ||= value
+      }
+    }
+  else
+    graph.take(index).each_with_index { |upper_row, i|
+      lower_row = graph[-(i + 1)]
+      lower_row.each_with_index { |lower_value, j| upper_row[j] ||= lower_value }
+    }
+    graph.slice!(index..)
+  end
+}
+
+graph.map { |row| row.map { |value| value.nil? ? "." : "#" }.join("")}.each { |rowstr| pp rowstr }
